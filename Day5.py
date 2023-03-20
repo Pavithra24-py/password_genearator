60d0bb150f9d19cd60b49598b27ced617f5efb83
Pavithra Muniyandi <72301729+Pavithra24-py@users.noreply.github.com>
2023-03-20 17:04:49 +0530

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -8,6 +8,18 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# easy_level
+password =""
+for n in range(1, nr_letters+1):
+  password += random.choice(letters)
+ for n in range(1, nr_symbols+1):
+  password += random.choice(symbols)
+ for n in range(1, nr_numbers+1):
+  password += random.choice(numbers)
+  
+ print(password)
+
+# Hard_level
 
 password_list = []
 for n in range(1, nr_letters+1):
